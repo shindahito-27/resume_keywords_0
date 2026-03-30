408d9d774db745781c13d5c3629601a0f8c9498f
shindahito-27 <pranaysharma27.ps@gmail.com>
2026-03-30 14:56:40 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 //Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 const upload = multer({dest: "uploads/"});
